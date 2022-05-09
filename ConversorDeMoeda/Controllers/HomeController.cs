441782b33aa91ddaaf081398bad16dcbed2edb01
LucasMarapodi <lucasmarapodi.dev@gmail.com>
2022-05-09 16:46:02 -0300

    First Commit

---
+++ ConversorDeMoeda/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using ConversorDeMoeda.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoeda.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public async Task<IActionResult> Index()
+         {
+             var httpClient = new HttpClient();
+
+             var json = httpClient.GetStringAsync("https://economia.awesomeapi.com.br/last/USD-BRL");
+
+             await json;
+
+            dynamic data = JObject.Parse(json.Result);
+
+            //Valores da API
+            double high = Convert.ToDouble(data.USDBRL.high);
+            double low = Convert.ToDouble(data.USDBRL.low);
+
+
+            ValorModel valorModel = new ValorModel();
+
+            //Media dos Valores da API
+            valorModel.ValorReal = Math.Round(((high + low) /2),2).ToString();
+
+
+
+
+
+            return View(valorModel);
+        }
+
+        [HttpPost]
+        public IActionResult index(ValorModel valorModel)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    //valorModel.ValorConvertido = (float.Parse(valorModel.ValorDolar) * float.Parse(valorModel.ValorReal)).ToString();
+
+                    valorModel.ValorConvertido = Math.Round((float.Parse(valorModel.ValorDolar) * float.Parse(valorModel.ValorReal)),2).ToString(); ;
+                 }
+                catch (Exception e)
+                {
+                    TempData["MensagemErro"] = "Erro: " +  e.Message;
+                }
+            }
+           
+
+
+            return View(valorModel);
+        }
+
+    }
+}
---
+++ ConversorDeMoeda/Models/ValorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConversorDeMoeda.Models
+{
+    public class ValorModel
+    {
+        [Required(ErrorMessage ="Insira uma valor.")]
+        [RegularExpression("^[0-9]*(?:\\,[0-9]*)?$", ErrorMessage ="Apenas numeros seguidos de virgula. Ex: 10,00")]
+        public string ValorDolar { get; set; }
+
+        public string ValorReal { get; set; }
+
+        public string ValorConvertido { get; set; }
+    }
+}
